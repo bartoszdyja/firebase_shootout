a5be98ee865ba004064093eac4f0c88c8e2cfb62
railsbeginner <bartosz.dyja@gmail.com>
2016-03-31 17:18:10 +0200

    Add firebase data management

--- server.rb
+++ server.rb
@@ -1,18 +1,31 @@
 require 'webrick'
+require 'json'
+require 'firebase'
 
 server = WEBrick::HTTPServer.new(Port: 1234)
 
 class Game < WEBrick::HTTPServlet::AbstractServlet
 
   class ShootoutGame
-    attr_reader :id
+    BASE_URI = 'https://resplendent-heat-1218.firebaseio.com/'
+    attr_reader :id, :turn
+    $firebase = Firebase::Client.new(BASE_URI)
 
     def initialize
       @id = __id__.to_s
+      @turn = %w(shoot save).sample
+      # @firebase = Firebase::Client.new(BASE_URI)
     end
 
-    def new_game
-      "From new game " + id
+    def start_game
+      call = $firebase.push("games", { id: id, turn: turn })
+      name = JSON.parse(call.response.body)['name'];
+      { game_id: name, player_turn: turn }.to_json
+    end
+
+    def self.find_game(game_id)
+      response = $firebase.get("games/#{game_id}")
+      response.body.values.to_s
     end
   end
 
@@ -23,8 +36,12 @@ class Game < WEBrick::HTTPServlet::AbstractServlet
     when '/'
       result = 'Welcome to the Game. Hit /start path to begin.'
     when '/start'
-      @game = ShootoutGame.new
-      result = @game.new_game
+      @game = ShootoutGame.new.start_game
+      result = @game
+    when '/show'
+      game_id = request.query['game_id']
+      @game = ShootoutGame.find_game(game_id)
+      result = @game
     else
       response.status = 404
       result = 'Page not found.'
@@ -32,7 +49,6 @@ class Game < WEBrick::HTTPServlet::AbstractServlet
     response.body = result
   end
 end
-
 
 server.mount '/', Game
 
