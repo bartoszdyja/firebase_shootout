d23af09e67a26b32b94f9a616a168fc7f1d4847c
railsbeginner <bartosz.dyja@gmail.com>
2016-03-31 12:44:51 +0200

    Add server scaffold

--- server.rb
+++ server.rb
@@ -3,14 +3,36 @@ require 'webrick'
 server = WEBrick::HTTPServer.new(Port: 1234)
 
 class Game < WEBrick::HTTPServlet::AbstractServlet
-  def do_GET request, response
-    # status, conte:nt_type, body = do_stuff_with request
 
+  class ShootoutGame
+    attr_reader :id
+
+    def initialize
+      @id = __id__.to_s
+    end
+
+    def new_game
+      "From new game " + id
+    end
+  end
+
+  def do_GET(request, response)
     response.status = 200
-    response['Content-Type'] = 'text/plain'
-    response.body = request.path
+    response['Content-Type'] = 'application/json'
+    case request.path
+    when '/'
+      result = 'Welcome to the Game. Hit /start path to begin.'
+    when '/start'
+      @game = ShootoutGame.new
+      result = @game.new_game
+    else
+      response.status = 404
+      result = 'Page not found.'
+    end
+    response.body = result
   end
 end
+
 
 server.mount '/', Game
 
